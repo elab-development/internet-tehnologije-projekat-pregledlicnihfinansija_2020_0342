2248178145d162217815e3b3c5bcfc6c8ccbabb1
zdravkovicnikola <nzdravkovic2001@gmail.com>
2024-07-12 10:50:38 +0200

    Added tables in Trasactions page

--- React/src/pages/Transactions.jsx
+++ React/src/pages/Transactions.jsx
@@ -4,6 +4,8 @@ import axiosInstance from "../server/axiosInstance";
 import useForm from "../hooks/useForm";
 
 const Transactions = () => {
+    const [expenses, setExpenses] = useState([]);
+    const [incomes, setIncomes] = useState([]);
     const user = JSON.parse(window.sessionStorage.getItem('user'));
     const [incomeCategories, setIncomeCategories] = useState([]);
     const [expenseCategories, setExpenseCategories] = useState([]);
@@ -19,6 +21,36 @@ const Transactions = () => {
         income_category_id: 1,
     });
 
+    const updateUser = () => {
+        axiosInstance.get("/profile").then((response) => {
+            console.log('user data');
+            console.log(response.data);
+            window.sessionStorage.setItem('user', JSON.stringify(response.data));
+        }).catch((error) => {
+            console.error(error);
+        });
+    }
+    const obrisiTrosak = (id) => {
+        axiosInstance.delete("/expenses/" + id).then((response) => {
+            console.log(response.data);
+            setForceUpdate(!forceUpdate);
+            updateUser();
+            setMessage("Uspesno obrisan trosak");
+        }).catch((error) => {
+            console.error(error);
+        });
+    }
+
+    const obrisiUplatu = (id) => {
+        axiosInstance.delete("/incomes/" + id).then((response) => {
+            console.log(response.data);
+            setForceUpdate(!forceUpdate);
+            updateUser();
+            setMessage("Uspesno obrisana uplata");
+        }).catch((error) => {
+            console.error(error);
+        });
+    }
     useEffect(() => {
         axiosInstance.get("/income_categories").then((response) => {
             console.log(response.data);
@@ -138,6 +170,70 @@ const Transactions = () => {
                     </Form.Group>
                     <hr />
                     <button onClick={dodajUplatu} className="btn btn-success">Dodaj uplatu</button>
+                </Col>
+            </Row>
+            <Row className="mt-3">
+                <Col>
+                    <h1 className="text-center">Troskovi</h1>
+                    <Table hover>
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Naziv</th>
+                            <th>Datum</th>
+                            <th>Iznos</th>
+                            <th>Kategorija</th>
+                            <th>Akcija</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            expenses && expenses.map((expense, index) => (
+                                <tr key={index}>
+                                    <td>{index + 1}</td>
+                                    <td>{expense.expenseName}</td>
+                                    <td>{expense.expenseDate}</td>
+                                    <td>{expense.expenseValue}</td>
+                                    <td>{expense.expenseCategory.categoryName}</td>
+                                    <td>
+                                        <button onClick={() => obrisiTrosak(expense.id)} className="btn btn-danger">Obrisi trosak</button>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                        </tbody>
+                    </Table>
+                </Col>
+                <Col>
+                    <h1 className="text-center">Prilivi</h1>
+                    <Table hover>
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Naziv</th>
+                            <th>Datum</th>
+                            <th>Iznos</th>
+                            <th>Kategorija</th>
+                            <th>Akcija</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            incomes && incomes.map((income, index) => (
+                                <tr key={index}>
+                                    <td>{index + 1}</td>
+                                    <td>{income.incomeName}</td>
+                                    <td>{income.incomeDate}</td>
+                                    <td>{income.incomeValue}</td>
+                                    <td>{income.incomeCategory.categoryName}</td>
+                                    <td>
+                                        <button onClick={() => obrisiUplatu(income.id)} className="btn btn-danger">Obrisi uplatu</button>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                        </tbody>
+                    </Table>
                 </Col>
             </Row>
         </div>
